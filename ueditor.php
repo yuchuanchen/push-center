6492761b838560db4498459cb8e4750543ad2080
chenkai <chenkai@meicai.cn>
2015-07-11 12:25:31 +0800

    remove space

--- ueditor.php
+++ ueditor.php
@@ -6,7 +6,7 @@
     <script type="text/javascript" charset="utf-8" src="ueditor/ueditor.all.min.js"></script>
     <!--建议手动加在语言，避免在ie下有时因为加载语言失败导致编辑器加载失败-->
     <!--这里加载的语言文件会覆盖你在配置项目里添加的语言类型，比如你在配置项目里配置的是英文，这里加载的中文，那最后就是中文-->
-    <script type="text/javascript" charset="utf-8" src="lang/zh-cn/zh-cn.js"></script>
+    <script type="text/javascript" charset="utf-8" src="ueditor/lang/zh-cn/zh-cn.js"></script>
 
     <style type="text/css">
         div {
@@ -64,7 +64,7 @@
         xmlhttp.setRequestHeader("Connection", "close");
         xmlhttp.send(str);
     }
-    //获取最新插入的htnl
+    //获取最新插入的html
     function getLastContent() {
         var xmlhttp;
         xmlhttp = GetXmlHttpObject();
